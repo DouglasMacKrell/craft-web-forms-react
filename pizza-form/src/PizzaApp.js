102a0316382613ca814bae3763346ef9aa2af7b6
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-09-18 10:19:12 -0400

    Pet Form structuring and Bootstrap load in.

--- pizza-form/src/PizzaApp.js
+++ pizza-form/src/PizzaApp.js
@@ -8,7 +8,6 @@ const validEmailRegex = RegExp(
 const validateForm = (errors) => {
   let valid = true;
   Object.values(errors).forEach(
-    //if we have an error string set valid to false
     (val) => val.length > 0 && (valid = false)
   );
 
